3759c9880ba54660a44328a4d78f2a22c7a1229e
MonEmperor <abr.da.king2016@gmail.com>
2022-07-08 13:29:50 -0400

    created db setup file
    meant to be a list of all commands to be run to create the database.
    
    run that file on your mysql database to set it up.

---
+++ src/server/db/db_config.sql
@@ -0,0 +1,2 @@
+USE Waycord;
+SELECT * FROM Users;
---
+++ src/server/db/db_setup.sql
@@ -0,0 +1,12 @@
+create DATABASE Waycord; -- or use it
+
+USE Waycord;
+
+-- insert the differet tables
+-- USERS
+--
+-- Messages
+--
+-- Servers
+--
+-- Channels?
