65fd742f9cc4cecd80b93b914706ca9af1c69b83
MonEmperor <abr.da.king2016@gmail.com>
2022-07-10 22:28:44 -0400

    dockerfile now works.
    will need to make revisions
    all env fiiles should be hidden preferably
    
    the mysql image/setup should be included somehow
    
    app still needs to be run as root somehow

--- src/server/db/connection.js
+++ src/server/db/connection.js
@@ -5,8 +5,8 @@ require('dotenv').config({path:path.resolve(__dirname, './../.env')})
 // TODO:
 //  develop this in a vm and use dev user n stuff
 module.exports = sql.createConnection({
-    host: `${process.env.HOST}`,
-    user: `${process.env.USER}`,
-    password: `${process.env.PASSWORD}`,
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
     database: 'Waycord'
 })
