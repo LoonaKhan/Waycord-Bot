6b0bbd99676615e83998bd44adb201d0f2d36f47
MonEmperor <abr.da.king2016@gmail.com>
2022-07-22 00:11:47 -0400

    messages endpoints added

--- src/server/routes/messages.js
+++ src/server/routes/messages.js
@@ -7,6 +7,7 @@
 
 const { Router } = require('express')
 const db = require('../db/connection')
+const Query = require('.././db/query')
 const path = require('path')
 require('dotenv').config({path: path.resolve(__dirname, '../.env')})
 
@@ -17,12 +18,77 @@ const router = Router()
 
 // get a message
 // whenever we get an archive?
+router.get('/:id', async (req, res) =>{
+
+    const { key } = req.headers
+    const {id} = req.params
+
+    if (key === process.env.KEY){
+        try {
+
+            db.query(`SELECT * FROM MESSAGES WHERE id = '${id}'`, (err, result) => {
+            if (err){
+                res.status(400).send(err)
+            }
+
+            res.status(201).send(result)
+
+            })
+
+        } catch (err) {
+            res.status(500).send(err)
+        }
+    }
+})
 
 // add a message
 // whenever we archive a message
+router.post('/', async (req, res) =>{
+
+    const { key } = req.headers
+    const { id, author, contents, channel, creation_date } = req.body
+
+    if (key === process.env.KEY){
+        try {
+
+            db.query(`INSERT INTO MESSAGES(id, author, contents, channel, creation_date) VALUES('${id}', '${author}', '${contents}', '${channel}', '${creation_date}')`, (err, result) => {
+            if (err){
+                res.status(400).send(err)
+            }
+
+            res.status(201).send(result)
+
+            })
+
+        } catch (err) {
+            res.status(500).send(err)
+        }
+    }
+})
 
 // delete a message
 // whenever we un-archive something, delete the corresponding message
+router.delete('/:id', async (req, res) => {
+    const { key } = req.headers
+    const { id } = req.params
+
+    if (key === process.env.KEY) {
+        try {
+
+            db.query(`DELETE FROM MESSAGES WHERE id = '${id}'`, (err, result) => {
+            if (err){
+                res.status(400).send(err)
+            }
+
+            res.status(201).send(result)
+
+            })
+
+        } catch (err) {
+            res.status(500).send(err)
+        }
+    }
+})
 
 
 
