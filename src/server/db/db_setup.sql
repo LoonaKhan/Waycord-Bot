b184f1ea34ee656404967a1955b0e7258d4e2b64
MonEmperor <abr.da.king2016@gmail.com>
2022-07-21 23:03:20 -0400

    deployed the SQL database.

--- src/server/db/db_setup.sql
+++ src/server/db/db_setup.sql
@@ -5,29 +5,32 @@ USE Waycord;
 
 
 -- Messages
+-- we only store the message id, author, contents and creation date.
+-- these are the absolute necessities that must be preserved.
+-- everything else can be accessed from discord's own db
+-- this does mean that if the message is deleted, all other data will be deleted however.
 CREATE TABLE MESSAGES(
     id INT NOT NULL UNIQUE,
     author INT NOT NULL, -- sender of the message
     contents TEXT NOT NULL, -- message contents, the text.
-    attachments TEXT NOT NULL, -- this is the url to any attachments. todo: MAKE IT AN ARRAY?
+    creation_date TEXT NOT NULL,
 
     -- what channel it was sent in.
     -- we only store the id and not a channel object
     -- since we can fetch the channel from discords own db
     channel INT NOT NULL
-
-    -- creation_time -- date created
 );
 
 -- Servers
 -- in order to keep anonymity, we only store certain bits of information.
 -- servers are mainly for analytics
 CREATE TABLE SERVERS(
-    id INT NOT NULL UNIQUE, -- make it auto increment? so that it has no connection to the server
+    id INT AUTO_INCREMENT,
     member_count INT NOT NULL,
-    creation_date date NOT NULL, -- todo: MAYBE JUST MAKE THIS TEXT? easier to encrypt
-    region VARCHAR(2) NOT NULL, -- might need to be longer
-    boost_level INT NOT NULL
+    creation_date TEXT NOT NULL,
+    boost_level INT NOT NULL,
+
+    PRIMARY KEY (id)
 );
 
 -- archive
@@ -38,7 +41,7 @@ CREATE TABLE ARCHIVES(
     title VARCHAR(255) NOT NULL,
     creator INT NOT NULL,
     message_id INT NOT NULL,
-    -- creation_time -- todo: date created
+    creation_date TEXT NOT NULL,
 
     PRIMARY KEY (id),
     FOREIGN KEY (message_id) REFERENCES MESSAGES(id)
