7532772ec600a543d53b42771c10a5a263fcab62
MonEmperor <46490084+MonEmperor@users.noreply.github.com>
2022-07-24 00:04:25 -0400

    server endpoints.
    done all endpoints now.
    
    will need to test them later before starting the bot and hosting.

--- src/server/routes/servers.js
+++ src/server/routes/servers.js
@@ -21,13 +21,45 @@ const router = Router()
 router.get('/:id',  async (req, res) => {
 
     const { key } = req.headers
-    if (key !== process.env.KEY) { // if the key is invalid
-        res.status(400).send() // just dont respond?
+    const {id} = req.params
+
+    if (key === process.env.KEY) { // if the key is invalid
+
+        try {
+            db.query(`SELECT * FROM SERVERS WHERE id = '${id}'`,
+            (err, result) => {
+                if (err) {
+                    res.status(400).send(err)
+                }
+
+                res.status(201).send(result)
+
+            })
+        } catch (err) {
+            res.status(500).send(err)
+        }
+
     }
+})
+
+// add a server
+// Used when the bot first joins a server.
+router.post('/', async (req, res) => {
+
+    const {key} = req.headers
+    const { member_count, creation_date, boost_level } = req.body
 
     try {
 
-        dbQuery(``)
+        db.query(`INSERT INTO SERVERS (member_count, creation_date, boost_level) VALUES('${member_count}', '${creation_date}', '${boost_level}')`,
+            (err, result) => {
+            if (err) {
+                res.status(400).send(err)
+            }
+
+            res.status(201).send(result)
+
+            })
 
     } catch (err) {
         res.status(500).send(err)
@@ -35,14 +67,61 @@ router.get('/:id',  async (req, res) => {
 
 })
 
-// add a server
-// Used when the bot first joins a server.
-
 // update a server
 // attempts to be used whenever a bot command is used in a server
+router.put('/:id', async (req, res) => {
+
+    const {key} = req.headers
+    const { id } = req.params
+    const { member_count, boost_level } = req.body
+
+    try {
+
+        db.query(`UPDATE SERVERS SET member_count = ${member_count}, boost_level = ${boost_level} WHERE id = '${id}'`,
+            (err, result) => {
+            if (err) {
+                res.status(400).send(err)
+            }
+
+            res.status(201).send(result)
+
+            })
+
+    } catch (err) {
+        res.status(500).send(err)
+    }
+
+})
 
 // delete a server
 // todo: allow the admins to blacklist a server from being tracked?
+router.delete('/:id', async  (req, res) => {
+
+    const { key } = req.headers
+    const { id } = req.params
+
+    if (key === process.env.KEY) {
+
+        try {
+
+            db.query(`DELETE FROM SERVERS WHERE id = '${id}'`,
+                (err, result) =>{
+
+                if (err) {
+                    res.status(400).send(err)
+                }
+
+                res.status(201).send(result)
+
+                })
+
+        } catch (err) {
+            res.status(500).send(err)
+        }
+
+    }
+
+})
 
 
 
