1d8f1e6214062d762f0ae2c6df77f91235eaed70
MonEmperor <46490084+MonEmperor@users.noreply.github.com>
2022-08-01 01:10:26 -0400

    removed server api calls from calls.py

--- src/waycord_bot/waycord_api/calls.py
+++ src/waycord_bot/waycord_api/calls.py
@@ -14,53 +14,9 @@ import json
 
 port = 4000
 base_url = f"http://localhost:{port}/"
-server_url = base_url + "servers/"
 message_url = base_url + "messages/"
 archive_url = base_url + "archives/"
 
-
-
-# Server calls
-def getServer(key:str, id:int):
-    # gets a api by ID
-    res = req.get(url=server_url+ f"{id}",
-                  headers={
-                      "key": key
-                  })
-    return json.loads(res.text)
-
-def addServer(key:str, member_count:int, creation_date:str, boost_level:int):
-    # adds a new api based on info given
-    res = req.post(url=server_url,
-                   headers={
-                       "key": key
-                   },
-                   data={
-                       "member_count": member_count,
-                       "creation_date": creation_date,
-                       "boost_level": boost_level
-                   })
-    return json.loads(res.text)
-
-def updateServer(key:str, id:int, member_count:int, boost_level:int):
-    # updates a api based on the info given
-    res = req.put(url=server_url + f"{id}",
-                  headers={
-                      "key": key
-                  },
-                  data={
-                      "member_count": member_count,
-                      "boost_level": boost_level
-                  })
-    return json.loads(res.text)
-
-def delServer(key:str, id:int):
-    # deletes a api by id
-    res = req.delete(url=server_url + f"{id}",
-                     headers={
-                         "key": key
-                     })
-    return json.loads(res.text)
 
 
 #  Messages calls
@@ -137,36 +93,6 @@ def delArchive(key:str, id:int):
 if __name__ == '__main__': # tests
     # Key test
     #print(getMsg(key="key", id=1)) # wrong key. dosent run
-
-    # getServer tests
-    # print(getServer(key="key", id=-1)) # nonexistent id. dosent run
-    # print(getServer(key="key", id=3)) # proper key and id. runs
-
-
-    # addServer
-    # Bad types here are fine since the user cannot interact with the servers
-
-    # proper. runs
-    #print(addServer(key="key", member_count=0, creation_date="sometime", boost_level=2))
-    # bad creation_date type. runs. but this is fine since the user cannot edit this
-    #print(addServer(key="key", member_count=0, creation_date=1, boost_level=2))
-    # bad member_count type: "0". runs since JS converts it automatically
-    #print(addServer(key="key", member_count="0", creation_date="sometime", boost_level=2))
-    # bad member_count type: "l". dosent run since JS cant convert "l" to a number
-    #print(addServer(key="key", member_count="l", creation_date="sometime", boost_level=2))
-
-
-    # delServer
-    # because all delete methods are the same, no other tests will be kept
-    #print(delServer(key="key", id=12)) # proper
-    #print(delServer(key="key", id=-10)) # non-existant id. runs, but nothing happens
-
-    # updateServer
-    #print(updateServer(key="key", id=9, member_count=10, boost_level=0)) # proper
-    #print(updateServer(key="key", id=1, member_count=10, boost_level=0)) # invalid id. runs, but dosent do anything
-    #print(updateServer(key="key", id=9, member_count=10, boost_level="g")) # invalid type "g". dosent run
-    #print(updateServer(key="key", id=9, member_count=11, boost_level="0")) # invalid type. runs because JS
-
 
     # getMsg tests
     #print(getMsg(key="key", id=-1)) # proper, runs
