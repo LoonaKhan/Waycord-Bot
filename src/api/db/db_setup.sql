bb0b2e5408a1b62b3386822870a3d30e41dad578
MonEmperor <46490084+MonEmperor@users.noreply.github.com>
2022-08-09 20:21:41 -0400

    uses strings instead of ints for discord id's
    javascript auto rounds large numbers, so in order to handle discord id's we must store them as strings instead

--- src/api/db/db_setup.sql
+++ src/api/db/db_setup.sql
@@ -10,25 +10,22 @@ USE Waycord;
 -- everything else can be accessed from discord's own db
 -- this does mean that if the message is deleted, all other data will be deleted however.
 CREATE TABLE MESSAGES(
-    id BIGINT NOT NULL UNIQUE,
-    author BIGINT NOT NULL, -- sender of the message
+    id VARCHAR(255) NOT NULL UNIQUE,
+    server VARCHAR(255) NOT NULL,
+    channel VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL, -- sender of the message
     contents TEXT NOT NULL, -- message contents, the text.
-    creation_date TEXT NOT NULL,
-
-    -- what channel it was sent in.
-    -- we only store the id and not a channel object
-    -- since we can fetch the channel from discords own db
-    channel BIGINT NOT NULL
+    creation_date TEXT NOT NULL
 );
 
 -- archive
 -- contains a message + title and other info to archive
 -- the main part of the app
 CREATE TABLE ARCHIVES(
-    id BIGINT AUTO_INCREMENT,
+    id INT AUTO_INCREMENT,
     title VARCHAR(255) NOT NULL,
-    creator BIGINT NOT NULL,
-    message_id BIGINT NOT NULL,
+    creator VARCHAR(255) NOT NULL,
+    message_id VARCHAR(255) NOT NULL,
     creation_date TEXT NOT NULL,
 
     PRIMARY KEY (id),
